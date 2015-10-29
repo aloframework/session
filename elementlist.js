873c348af682989f91f219d9f8580d9e7b3276c1
Alorel <a.molcanovas@gmail.com>
2015-10-29 17:52:17 +0000

    no event

--- elementlist.js
+++ elementlist.js
@@ -1,2 +1,3 @@
+
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c", "AloFramework\\Session\\AbstractSession"], ["c", "AloFramework\\Session\\Config"], ["c", "AloFramework\\Session\\MySQLSession"], ["c", "AloFramework\\Session\\RedisSession"], ["c", "AloFramework\\Session\\SessionException"], ["c", "ArrayAccess"], ["c", "Exception"], ["c", "PDO"], ["c", "Redis"], ["c", "SessionHandlerInterface"]];
+ApiGen.elements = [["c","AloFramework\\Session\\AbstractSession"],["c","AloFramework\\Session\\Config"],["c","AloFramework\\Session\\MySQLNoEventSession"],["c","AloFramework\\Session\\MySQLSession"],["c","AloFramework\\Session\\RedisSession"],["c","AloFramework\\Session\\SessionException"],["c","ArrayAccess"],["c","Exception"],["c","PDO"],["c","Redis"],["c","SessionHandlerInterface"]];
